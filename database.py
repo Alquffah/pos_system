ff07455658902729c43e2f7fa39ba83398105d79
Zakariya <z.alquffah@gmail.com>
2022-12-12 23:16:08 -0500

    update2

--- database.py
+++ database.py
@@ -2,13 +2,24 @@ import sqlite3
 conn = sqlite3.connect('product.db')
 #c = sqlite3.connect(':memory:') # database in memory
 c = conn.cursor()
-product_name = 'item1'
-product_price = '5'
-product_cost = '4'
-product_profit = '1'
-product_quantity = '10'
-product_bar_code = '123456789012'
-c.execute("INSERT INTO products (product_name, product_price, product_cost, product_profit, product_quantity, product_bar_code) VALUES (?,?,?,?,?,?)", (product_name, product_price, product_cost, product_profit, product_quantity, product_bar_code))
+class Product:
+    def __init__(self, name, sale, cost, qua, barcode):
+        self.name = name
+        self.sale = sale
+        self.cost = cost
+        self.qua = qua
+        self.barcode = barcode
+
+    def profit(self):
+        return self.sale - self.cost
+
+    def qua_edit(self, num):
+        self.qua += num
+
+p1 = Product('cleaner', 6, 4, 12, 123456789012)
+p1.qua_edit(3)
+
+c.execute("INSERT INTO products (name, price, cost, profit, quantity, bar_code) VALUES (?,?,?,?,?,?)", (p1.name, p1.sale, p1.cost, p1.profit(), p1.qua, p1.barcode))
 
 conn.commit()
 conn.close()
