db65b8b2d20d66119d0a4afc9ec8053c130838a7
Zakariya <z.alquffah@gmail.com>
2022-12-30 14:22:24 -0500

    restructured the interface.py and the database.py files to be more OOP friendly

--- database.py
+++ database.py
@@ -1,21 +1,48 @@
+from dataclasses import dataclass
 import sqlite3
 
+class table:
+    def __init__(self):
+        self.conn = sqlite3.connect('database.db')
+        self.cursor = self.conn.cursor()
+
+    def create_table(self):
+        self.cursor.execute("""CREATE TABLE IF NOT EXISTS items (
+            name TEXT,
+            price REAL,
+            cost REAL,
+            profit REAL,
+            quantity INTEGER,
+            bar_code TEXT
+        )""")
+
+    def delete_item(self, id):
+        self.cursor.execute("DELETE from items WHERE oid = (?)", [id])
+        self.conn.commit()
+
+    def records(self):
+        self.cursor.execute("SELECT *, oid FROM items")
+        return self.cursor.fetchall()
+
+    def __del__(self):
+        self.conn.close()
+
 #c = sqlite3.connect(':memory:') # database in memory
+@dataclass
 class Product:
-    db_name = 'database.db'
-    def __init__(self, name, sale, cost, profit, qua, barcode):
-        self.name = name
-        self.sale = sale
-        self.cost = cost
-        self.qua = qua
-        self.barcode = barcode
-        self.profit = profit
+    name: str
+    cost: float
+    sale: float
+    profit: float
+    qua: int
+    barcode: str
+    id: str
 
     def qua_edit(self, num):
         self.qua += num
 
     def run_query(self, query, parameters = ()):
-        with sqlite3.connect(self.db_name) as conn:
+        with sqlite3.connect('database.db') as conn:
             cursor = conn.cursor()
             result = cursor.execute(query, parameters)
             conn.commit()
@@ -28,33 +55,10 @@ class Product:
         parameters = (self.name, self.sale, self.cost, self.profit, self.qua, self.barcode)
         self.run_query(query, parameters)
 
-
-def edit_item(name, price, cost, profit, quantity, bar_code, id):
-    conn = sqlite3.connect('database.db')
-    c = conn.cursor()
-    c.execute("UPDATE items SET name = ?, price = ?, cost = ?, profit = ?, quantity = ?, bar_code = ? WHERE oid = ?", (str(name), float(price), float(cost), float(profit), int(quantity), str(bar_code),str(id)))
-    conn.commit()
-    conn.close()
-
-def delete_item(id):
-    conn = sqlite3.connect('database.db')
-    c = conn.cursor()
-    c.execute("DELETE from items WHERE oid = (?)", [id])
-    conn.commit()
-    conn.close()
-
-
-def records():
-    conn = sqlite3.connect('database.db')
-    c = conn.cursor()
-    c.execute("SELECT *, oid FROM items")
-    result = c.fetchall()
-    conn.commit()
-    conn.close()
-    return result       
-
-"""
-p1 = Product('cleaner', 6, 4, 2, 12, 123456789012)
-#p1.qua_edit(3)
-p1.add_item()
-"""
+    def edit_item(self):
+        query = "UPDATE items SET name = ?, price = ?, cost = ?, profit = ?, quantity = ?, bar_code = ? WHERE oid = ?"
+        parameters = (self.name, self.sale, self.cost, self.profit, self.qua, self.barcode, self.id)
+        self.run_query(query, parameters)
+ 
+class customer:
+    pass
--- interface.py
+++ interface.py
@@ -1,183 +1,213 @@
 import tkinter as tk                    
 from tkinter import ttk
 import database
+
+
+class App(tk.Tk):
+    def __init__(self,*args,**kwargs):
+        tk.Tk.__init__(self,*args,**kwargs)
+        self.notebook = ttk.Notebook()
+        self.add_tab()
+        self.notebook.grid(row=0)
+        self.selected_item_id = None
+        #self.notebook.pack(expand = True, fill ="both")
   
-root = tk.Tk(className = " POS System")
-root.geometry("700x700")
-# root.attributes('-fullscreen', True)
-# root.resizable(0,0) # fix window size
+    def add_tab(self):
+        tab1 = items_database(self.notebook)
+        tab2 = cashier(self.notebook)
+        tab3 = history(self.notebook)
+        tab4 = settings(self.notebook)
+        self.notebook.add(tab1,text="Database")
+        self.notebook.add(tab2,text="Cashier")
+        self.notebook.add(tab3,text="History")
+        self.notebook.add(tab4,text="Settings")
 
-# Tabs creation
-tabControl = ttk.Notebook(root)
-tab1 = ttk.Frame(tabControl)
-#tab1.grid(row=3, column=10)
-tab2 = ttk.Frame(tabControl)
-tab3 = ttk.Frame(tabControl)
-tab4 = ttk.Frame(tabControl)
-tabControl.add(tab1, text ='Products Database')
-tabControl.add(tab2, text ='Cashier')
-tabControl.add(tab3, text ='History')
-tabControl.add(tab4, text ='Settings')
-tabControl.pack(expand = True, fill ="both")
-  
-# Item data input section in 1st tab
-ttk.Label(tab1, text ="Item Name").grid(row = 0, column= 0, padx = 10)
-ttk.Label(tab1, text ="Cost Price").grid(row = 0, column= 1, padx = 10)
-ttk.Label(tab1, text ="Sale Price").grid(row = 0, column= 2, padx = 10)
-ttk.Label(tab1, text ="Profit").grid(row = 0, column= 3, padx = 10)
-ttk.Label(tab1, text ="Quantity").grid(row = 0, column= 4, padx = 10)
-ttk.Label(tab1, text ="Barcode").grid(row = 0, column= 5, padx = 10)
+class items_database(ttk.Frame):
+    def __init__(self,name,*args,**kwargs):
+        ttk.Frame.__init__(self,*args,**kwargs)
+        #self.name = name
 
-name_var, barcode_var = tk.StringVar(), tk.StringVar()
-cost_var, sale_var, profit_var, qua_var = tk.DoubleVar(), tk.DoubleVar(), tk.DoubleVar(), tk.IntVar()
+        # Labels in the Database tab
+        ttk.Label(self, text ="Item Name").grid(row = 0, column= 0, padx = 10)
+        ttk.Label(self, text ="Cost Price").grid(row = 0, column= 1, padx = 10)
+        ttk.Label(self, text ="Sale Price").grid(row = 0, column= 2, padx = 10)
+        ttk.Label(self, text ="Profit").grid(row = 0, column= 3, padx = 10)
+        ttk.Label(self, text ="Quantity").grid(row = 0, column= 4, padx = 10)
+        ttk.Label(self, text ="Barcode").grid(row = 0, column= 5, padx = 10)
+        ttk.Label(self, text ="Products Database", font= 30).grid(row = 3, column= 0, columnspan = 6)
+        self.added_label = ttk.Label(self, text ="")
+        self.added_label.grid(row = 2, column= 3, columnspan = 5)
 
-item_name_input = ttk.Entry(tab1, width = 10, textvariable=name_var)
-item_name_input.grid(row = 1, column= 0, padx = 10)
+        #Entry boxes in the Database tab
+        self.name_var, self.barcode_var = tk.StringVar(), tk.StringVar()
+        self.cost_var, self.sale_var, self.profit_var, self.qua_var = tk.DoubleVar(), tk.DoubleVar(), tk.DoubleVar(), tk.IntVar()
 
-item_cost_input = ttk.Entry(tab1, width = 10, textvariable=cost_var)
-item_cost_input.grid(row = 1, column= 1, padx = 10)
+        self.item_name_input = ttk.Entry(self, width = 10, textvariable=self.name_var)
+        self.item_name_input.grid(row = 1, column= 0, padx = 10)
 
-item_sale_input = ttk.Entry(tab1, width = 10, textvariable=sale_var)
-item_sale_input.grid(row = 1, column= 2, padx = 10)
+        self.item_cost_input = ttk.Entry(self, width = 10, textvariable=self.cost_var)
+        self.item_cost_input.grid(row = 1, column= 1, padx = 10)
 
-item_profit_input = ttk.Entry(tab1, width = 10, textvariable=profit_var, state="readonly")
-item_profit_input.grid(row = 1, column= 3, padx = 10)
+        self.item_sale_input = ttk.Entry(self, width = 10, textvariable=self.sale_var)
+        self.item_sale_input.grid(row = 1, column= 2, padx = 10)
 
-item_qua_input = ttk.Entry(tab1, width = 10, textvariable=qua_var)
-item_qua_input.grid(row = 1, column= 4, padx = 10)
+        self.item_profit_input = ttk.Entry(self, width = 10, textvariable=self.profit_var, state="readonly")
+        self.item_profit_input.grid(row = 1, column= 3, padx = 10)
+        # Trace Od and Do, call count_working_hours on change
+        self.cost_var.trace("w", self.calculate_profit)
+        self.sale_var.trace("w", self.calculate_profit)
 
-item_barcode_input = ttk.Entry(tab1, width = 10, textvariable=barcode_var)
-item_barcode_input.grid(row = 1, column= 5, padx = 10)
+        self.item_qua_input = ttk.Entry(self, width = 10, textvariable=self.qua_var)
+        self.item_qua_input.grid(row = 1, column= 4, padx = 10)
 
-def calculate_profit(*args):
-    global cost_var, sale_var, profit_var
+        self.item_barcode_input = ttk.Entry(self, width = 10, textvariable=self.barcode_var)
+        self.item_barcode_input.grid(row = 1, column= 5, padx = 10)
 
-    try:
-        # Get sale and cost values (should be integer)
-        cost = cost_var.get()
-        sale = sale_var.get()
-        profit = sale - cost
-    except (tk.TclError):
-        # In case of invalid input, set profit to 0
-        profit = 0
-    finally:
-        # Set profit
-        profit_var.set(profit)
+        self.data_tree = self.create_data_tree()
+        self.show_database()
 
 
-# Trace Od and Do, call count_working_hours on change
-cost_var.trace("w", calculate_profit)
-sale_var.trace("w", calculate_profit)
+        #Buttons in the Database tab
+        clear_button = ttk.Button(self, text = "Clear", command=self.clear_entries)
+        clear_button.grid(row = 2, column= 1, pady = 10)
 
-def store_data():
-    global item_name_input, item_cost_input, item_sale_input, item_profit_input, item_qua_input, item_barcode_input
-    database.Product(item_name_input.get(), item_cost_input.get(), item_sale_input.get(), item_profit_input.get(), item_qua_input.get(), item_barcode_input.get()).add_item()
-    message = "Added " + str(item_name_input.get()) + " to the database"
-    ttk.Label(tab1, text =message).grid(row = 2, column= 3, columnspan = 5)
-    show_database()
+        submit_button = ttk.Button(self, text = "Add", command=self.store_data)
+        submit_button.grid(row = 2, column= 0, pady = 10)
 
-submit_button = ttk.Button(tab1, text = "Add", command=store_data)
-submit_button.grid(row = 2, column= 0, pady = 10)
+        delete_button = ttk.Button(self, text = "Delete", command=self.delete_item1)
+        delete_button.grid(row = 5, column= 0, pady = 10)
 
-def clear_entries():
-	# Clear entry boxes
-	item_name_input.delete(0, 'end')
-	item_cost_input.delete(0, 'end')
-	item_sale_input.delete(0, 'end')
-	item_profit_input.delete(0, 'end')
-	item_qua_input.delete(0, 'end')
-	item_barcode_input.delete(0, 'end')
+        edit_button = ttk.Button(self, text = "Submit Edit", command=self.edit_item1)
+        edit_button.grid(row = 2, column= 2, pady = 10)
 
-clear_button = ttk.Button(tab1, text = "Clear", command=clear_entries)
-clear_button.grid(row = 2, column= 1, pady = 10)
+        edit_button = ttk.Button(self, text = "View history", command=self.item_history_window)
+        edit_button.grid(row = 5, column= 1, pady = 10)
 
+    #Scrollbars
+        verscrlbar = ttk.Scrollbar(self,
+                                orient ="vertical",
+                                command = self.data_tree.yview)
+        verscrlbar.grid(row = 4, column= 6)
+        self.data_tree.configure(xscrollcommand = verscrlbar.set)
 
-ttk.Label(tab1, text ="Products Database", font= 30).grid(row = 3, column= 0, columnspan = 6)
+    #Functions
+    def calculate_profit(self, *args):
 
-data_tree = ttk.Treeview(tab1)
-data_tree["columns"] = ("name", "Sale", "Cost", "Profit", "Quantity", "Barcode", "id")
-data_tree.column("#0", width = 0)
-data_tree.column("name", width = 150)
-data_tree.column("Sale", width = 50)
-data_tree.column("Cost", width = 50)
-data_tree.column("Profit", width = 50)
-data_tree.column("Quantity", width = 60)
-data_tree.column("Barcode", width = 150)
-data_tree.column("id", width = 50)
+        try:
+            # Get sale and cost values (should be integer)
+            cost = self.cost_var.get()
+            sale = self.sale_var.get()
+            profit = sale - cost
+        except (tk.TclError):
+            # In case of invalid input, set profit to 0
+            profit = 0
+        finally:
+            # Set profit
+            self.profit_var.set(profit)
 
-data_tree.heading("name", text = "item name")
-data_tree.heading("Sale", text = "Sale")
-data_tree.heading("Cost", text = "Cost")
-data_tree.heading("Profit", text = "Profit")
-data_tree.heading("Quantity", text = "Quantity")
-data_tree.heading("Barcode", text = "Barcode")
-data_tree.heading("id", text = "id")
-
-data_tree.grid(row = 4, column= 0, columnspan = 6, padx=20)
-
-def show_database():
-    data_tree.delete(*data_tree.get_children())
-    items_list = database.records()
-    for record in items_list:
-        data_tree.insert(parent='', index='end', text='', values=(record[0], record[1], record[2], record[3], record[4], record[5], record[6]))
-
-show_database()
-
-verscrlbar = ttk.Scrollbar(tab1,
-                           orient ="vertical",
-                           command = data_tree.yview)
-verscrlbar.grid(row = 4, column= 6)
- 
-# Configuring treeview
-data_tree.configure(xscrollcommand = verscrlbar.set)
-
-selected_item_id = None
-
-def selectItem(a):
-    global selected_item_id
+            
+    def clear_entries(self):
+        # Clear entry boxes
+        self.item_name_input.delete(0, 'end')
+        self.item_cost_input.delete(0, 'end')
+        self.item_sale_input.delete(0, 'end')
+        self.item_profit_input.delete(0, 'end')
+        self.item_qua_input.delete(0, 'end')
+        self.item_barcode_input.delete(0, 'end')
     
-    curItem = data_tree.focus()
-    selected_item_id = data_tree.item(curItem)["values"][6]
+    def store_data(self):
+        database.Product(self.item_name_input.get(), self.item_cost_input.get(), self.item_sale_input.get(), self.item_profit_input.get(), self.item_qua_input.get(), self.item_barcode_input.get(), "").add_item()
+        message = "Added " + str(self.item_name_input.get()) + " to the database"
+        self.added_label.config(text = message)
+        self.show_database()
+    
+    def create_data_tree(self):
+        #Treeview in the Database view
+        self.data_tree = ttk.Treeview(self)
+        self.data_tree["columns"] = ("name", "Sale", "Cost", "Profit", "Quantity", "Barcode", "id")
+        self.data_tree.column("#0", width = 0)
+        self.data_tree.column("name", width = 150)
+        self.data_tree.column("Sale", width = 50)
+        self.data_tree.column("Cost", width = 50)
+        self. data_tree.column("Profit", width = 50)
+        self.data_tree.column("Quantity", width = 60)
+        self.data_tree.column("Barcode", width = 150)
+        self.data_tree.column("id", width = 50)
 
-    name_var.set(data_tree.item(curItem)["values"][0])
-    cost_var.set(data_tree.item(curItem)["values"][1])
-    sale_var.set(data_tree.item(curItem)["values"][2])
-    #item_profit_input = data_tree.item(curItem)["values"][3]
-    qua_var.set(data_tree.item(curItem)["values"][4])
-    barcode_var.set(data_tree.item(curItem)["values"][5])
+        self.data_tree.heading("name", text = "item name")
+        self.data_tree.heading("Sale", text = "Sale")
+        self.data_tree.heading("Cost", text = "Cost")
+        self.data_tree.heading("Profit", text = "Profit")
+        self.data_tree.heading("Quantity", text = "Quantity")
+        self.data_tree.heading("Barcode", text = "Barcode")
+        self.data_tree.heading("id", text = "id")
 
-    print(item_name_input)
+        self.data_tree.bind('<ButtonRelease-1>', self.selectItem)
+        self.data_tree.grid(row = 4, column= 0, columnspan = 6, padx=20)
+        
 
-data_tree.bind('<ButtonRelease-1>', selectItem)
+        return self.data_tree
 
-def delete_item1():
-    selected_item = data_tree.selection()[0] ## get selected item
-    data_tree.delete(selected_item) 
-    #print(selectItem)
-    database.delete_item(selected_item_id)  
+    def show_database(self):
+        self.data_tree.delete(*self.data_tree.get_children())
+        items_list = database.table().records()
+        for record in items_list:
+            self.data_tree.insert(parent='', index='end', text='', values=(record[0], record[1], record[2], record[3], record[4], record[5], record[6]))
+    
 
-delete_button = ttk.Button(tab1, text = "Delete", command=delete_item1)
-delete_button.grid(row = 5, column= 0, pady = 10)
+    def selectItem(self,a):
+        curItem = self.data_tree.focus()
+        
+        self.name_var.set(self.data_tree.item(curItem)["values"][0])
+        self.cost_var.set(self.data_tree.item(curItem)["values"][1])
+        self.sale_var.set(self.data_tree.item(curItem)["values"][2])
+        self.qua_var.set(self.data_tree.item(curItem)["values"][4])
+        self.barcode_var.set(self.data_tree.item(curItem)["values"][5])
+        self.selected_item_id = self.data_tree.item(curItem)["values"][6]
 
-def edit_item1():
-    global item_name_input, item_cost_input, item_sale_input, item_profit_input, item_qua_input, item_barcode_input
+    def delete_item1(self):
+        selected_item = self.data_tree.selection()[0] # get selected item
+        self.data_tree.delete(selected_item) 
+        database.table().delete_item(self.selected_item_id)
 
-    database.edit_item(item_name_input.get(), item_cost_input.get(), item_sale_input.get(), item_profit_input.get(), item_qua_input.get(), item_barcode_input.get(), selected_item_id)
+    def edit_item1(self):
+        database.Product(self.item_name_input.get(), self.item_cost_input.get(), self.item_sale_input.get(), self.item_profit_input.get(), self.item_qua_input.get(), self.item_barcode_input.get(), self.selected_item_id).edit_item()
+        message = "Editted!"
+        self.added_label.config(text = message)
+        self.show_database()
 
-    show_database()
+    def item_history_window(self):
 
-edit_button = ttk.Button(tab1, text = "Submit Edit", command=edit_item1)
-edit_button.grid(row = 2, column= 2, pady = 10)
+        newWindow = tk.Toplevel(my_app)
 
-def item_history_window():
+        newWindow.title("Item History")
+    
+        newWindow.geometry("550x200")
 
-    newWindow = tk.Toplevel(root)
-
-    newWindow.title("Item History")
  
-    newWindow.geometry("550x200")
+class cashier(ttk.Frame):
+    def __init__(self,name,*args,**kwargs):
+        ttk.Frame.__init__(self,*args,**kwargs)
+        self.label = ttk.Label(self, text="Hi This is Tab2")
+        self.label.grid(row=1,column=0,padx=10,pady=10)
 
-edit_button = ttk.Button(tab1, text = "View history", command=item_history_window)
-edit_button.grid(row = 5, column= 1, pady = 10)
 
-root.mainloop()
+class history(ttk.Frame):
+   def __init__(self,name,*args,**kwargs):
+       ttk.Frame.__init__(self,*args,**kwargs)
+       self.label = ttk.Label(self, text="Hi This is Tab3")
+       self.label.grid(row=1,column=0,padx=10,pady=10)
+
+class settings(ttk.Frame):
+   def __init__(self,name,*args,**kwargs):
+       ttk.Frame.__init__(self,*args,**kwargs)
+       self.label = ttk.Label(self, text="Hi This is Tab4")
+       self.label.grid(row=1,column=0,padx=10,pady=10)
+
+my_app = App()
+my_app.title(" POS System")
+my_app.geometry("700x700")
+#my_app.attributes('-fullscreen', True)
+# my_app.resizable(0,0) # fix window size
+
+my_app.mainloop()
